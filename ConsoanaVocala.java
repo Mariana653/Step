15707e5809db1bf43ad3db0fac8c7646370585be
Cebotari  Mariana <makhu98@mail.ru>
2020-02-24 20:19:40 +0200

    ConsoanaVocala

---
+++ ConsoanaVocala.java
@@ -0,0 +1,15 @@
+public class ConsoanaVocala{
+
+	public static void main(String[]args){
+	char firstAlp='a';
+	
+	if((firstAlp=='a')||(firstAlp=='e')||(firstAlp=='i')||(firstAlp=='o')||(firstAlp=='u')){
+	System.out.println("Vocala");
+	
+	}
+	else{
+	System.out.println("Consoana");
+	
+	}
+	}
+}
