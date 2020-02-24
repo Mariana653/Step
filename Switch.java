7f4d37fd6c876017fbc7141f1e20979bf9846a5d
Cebotari  Mariana <makhu98@mail.ru>
2020-02-24 20:13:00 +0200

    Switch

---
+++ Switch.java
@@ -0,0 +1,32 @@
+public class Switch{
+	public static void main(String[] args){
+	char primaLit='c';
+
+	switch(primaLit){
+	case 'a':{
+	System.out.println("Vocala");
+        break;
+	}
+	case 'e':{
+	System.out.println("Vocala");
+	break;
+	}
+	case 'i':{
+	System.out.println("Vocala");
+	break;
+	}
+	case 'o':{
+	System.out.println("Vocala");
+	break;
+	}
+	case 'u':{
+	System.out.println("Vocala");
+	break;
+	}
+	default:{
+	System.out.println("Consoana");
+	}
+	}
+
+	}
+}
